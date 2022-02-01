0f69fce530124dbd71d3561a49b2a2baf5ada3b9
sungwon-097 <csw2757@gmail.com>
2022-02-01 21:03:04 +0900

    add security

--- restful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import java.net.Authenticator;
 
+@EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // update
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
         throws  Exception{
