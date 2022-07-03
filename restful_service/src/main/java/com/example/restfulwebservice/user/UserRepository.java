e5f976d4d432772d312d43a806589f749efedce2
sungwon-097 <csw2757@naver.com>
2022-07-03 12:52:34 +0900

    restful : use new directory

--- restful_service/src/main/java/com/example/restfulwebservice/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restfulwebservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository // spring bean의 타입
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
-// 인터페이스 생성만으로 CRUD 에 관련된 메소드 사용 가능
