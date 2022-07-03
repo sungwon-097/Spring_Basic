cf1b6f12d722498e65384b21b4f41f1c712ecce7
Sungwon Choi <66061268+sungwon-097@users.noreply.github.com>
2022-07-03 12:54:24 +0900

    Delete restful_service directory

--- restful_service/src/main/java/com/example/restfulwebservice/user/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.restfulwebservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Integer> { // 인터페이스 선언만으로 기본적인 select, insert 사용 가능
-}
