044ad1af83c41f298bfa35c5ae53d6be677fdc50
sungwon-097 <csw2757@naver.com>
2022-07-03 14:32:21 +0900

    restful : swagger model property

--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -6,16 +6,38 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-//    http://localhost:8088/swagger-ui/index.html#/
+    // contact 와 apiInfo 는  한 번 선언 후 바꿀 일이 없기 때문에 private static final 로 선언
+    private static final Contact DEFAULT_CONTACT = new Contact("Kennith Lee",
+        "http://localhost:8088/swagger-ui/index.html#/", "sungwon@github.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome ALI title",
+            "My User management REST_APi service", "1.0", "uniformResourceName:turmsOfService",
+            DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LISENCE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml")); // 일반 배열 형태의 List 를 ArrayList 로 변환한다.
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30);
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,15 +18,23 @@ import java.util.Date;
 @NoArgsConstructor // default constructor 가 존재하지 않아 상속 불가하기 때문에 선언해줌
 //@JsonIgnoreProperties(value={"password", "ssn"}) // class block 으로 한 번에 ignore
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요.") // method 에 @Valid 선언, valid 조건과 error message 선언
+    @ApiModelProperty(notes = "사용자 이름을 입력 해 주세요")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "사용의 등록을 입력 해 주세요")
     private Date joinDate;
+
 //    @JsonIgnore // 각각의 json 전달값을 무시(보안측면)
+    @ApiModelProperty(notes = "사용자 패스워드를 입력 해 주세요")
     private String password;
+
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력 해 주세요")
     private String ssn;
 }
