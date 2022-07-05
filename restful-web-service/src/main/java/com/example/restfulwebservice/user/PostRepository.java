3ac59879630d32c11a705654a6995f2758973f9d
Sungwon Choi <66061268+sungwon-097@users.noreply.github.com>
2022-07-05 21:49:58 +0900

    Revert "Sungwon"

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restfulwebservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository // database 와 관련된 bean
-public interface PostRepository extends JpaRepository<Post, Integer> {
-    // 상속된 인터페이스 선언으로 사용
-}
