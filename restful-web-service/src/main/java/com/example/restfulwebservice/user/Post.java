33b609cf8ccf9d9f9833d8d9a5d05b47d08f8c9a
sungwon-097 <csw2757@gmail.com>
2022-07-06 17:17:27 +0900

    restful : fixed

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/Post.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/Post.java
@@ -20,7 +20,7 @@ public class Post {
     private String description;
 
     // User : Post -> parent : child
-    @ManyToOne(fetch = FetchType.LAZY) // post 값이 여러 개가 올 수 있고 하나의 값과 매칭됨(게시물의 게시자는 하나)
+    @ManyToOne(fetch = FetchType.LAZY) // post 값이 여러 개가 올 수 있고 하나의 값과 매칭됨(게시물의 게시자는 하나, 지연로딩 lazy하게 불러옴)
     // post 데이터가 로딩되는 시점에 필요한 사용자 데이터를 가져(post entity 에서 바로 가져오지 않음)
     @JsonIgnore // 외부에 json 데이터가 공개되지 않음
     private User user;
